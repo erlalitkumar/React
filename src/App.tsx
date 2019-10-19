8bff4d939abe87155b0a04ab476d12817c2f214d
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-10-19 17:02:00 +0530

    simple and clean base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends React.Component {
-  sum(a: number, b: number): number {
-    return a + b;
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello world {this.sum(5,7)}</h1>
-      </div>
-    );
-  }
-}
+import React from "react";
 
-export default App;
+export default function App() {
+  return <div>
+    <h1>Hello world!</h1>
+  </div>;
+}
