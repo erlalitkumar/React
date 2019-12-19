5b947c5753e6bf8a0c73734b7ebcefe15cec9703
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-12-19 23:24:14 +0530

    react redux simple example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 class App extends React.Component {
+
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello world!</h1>
+      <div className="App" style={{ flex: 1 }}>
+        <div>Age:<span>{this.props.age}</span></div>
+        <button onClick={this.props.onAgeUp}>Age Up</button>
+        <button onClick={this.props.onAgeDown}>Age Down</button>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    age: state.age
+  }
+}
+const mapDispachToProps = (dispach) => {
+  return {
+    onAgeUp: () => dispach({ type: 'AGE_UP' }),
+    onAgeDown: () => dispach({ type: 'AGE_DOWN' })
+  }
+}
+export default connect(mapStateToProps, mapDispachToProps)(App);
