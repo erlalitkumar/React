918dca44028f0b9c09d7118af8019fd64590cd44
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-12-19 21:54:00 +0530

    redux simple example

---
+++ reduxtest.js
@@ -0,0 +1,24 @@
+const { createStore } = require('redux');
+
+const initialState = {
+    age: 21
+}
+
+const myReducer = (state = initialState, action) => {
+    const newState = {...state};
+    if (action.type === 'ADD') {
+        newState.age += 1;
+    }
+    if (action.type === 'SUBSTRACT') {
+        newState.age -= 1;
+    }
+
+    return newState;
+};
+
+const store = createStore(myReducer);
+store.subscribe(()=>{
+console.log(`state has changed ${JSON.stringify(store.getState())}`); 
+})
+store.dispatch({ type: 'ADD' });
+store.dispatch({ type: 'SUBSTRACT' });
