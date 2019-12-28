83ccea45bdef9ce001cdb931c2550ebde071e06d
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-12-28 14:31:21 +0530

    redux demo created with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+
+//Store
+
+//Reducer
+const countReducer = (state = 0, action:Action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+//Action
+
+interface Action {
+    type: string
+}
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+const store = createStore(countReducer)
+store.subscribe(()=>console.log(store.getState()))
+//Dispatch
+store.dispatch(increment())
+store.dispatch(decrement())
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
