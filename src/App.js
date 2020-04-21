3f4e4c199794d0101312434973ee2822321d5f48
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-04-21 14:15:53 +0530

    code refactored

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+
 class App extends React.Component {
-
-
   render() {
     return (
       <div className="App" style={{ flex: 1 }}>
