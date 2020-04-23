661a78d16742082969df04cd5ba63d16fc420b04
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-04-23 14:01:48 +0530

    myapp_ts: bootstrap added

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Timer from './components/Timer'
-import Net from './components/Net'
+import {Container, Row, Col, Image} from 'react-bootstrap';
 
-import React, { Fragment, useState } from "react";
-type FormElem = React.FormEvent<HTMLFormElement>;
-interface ITodo {
-  text: string;
-  complete: boolean;
-}
+import React, {Fragment, useState} from "react";
 
 export default function App(): JSX.Element {
-  const [value, setValue] = useState<string>("");
-  const [todos, setTodos] = useState<ITodo[]>([]);
-  const handleSubmit = (e: FormElem): void => {
-    e.preventDefault();
-    addTodo(value);
-    setValue("");
-  };
-
-  const addTodo = (text: string): void => {
-    const newTodos: ITodo[] = [...todos, { text, complete: false }];
-    setTodos(newTodos);
-  };
-
-  const removeTodo = (index: number): void => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
-  const completTodos = (index: number): void => {
-    const newTodos = [...todos];
-    newTodos[index].complete = !newTodos[index].complete;
-    setTodos(newTodos);
-  };
   return (
-    <Fragment>
-      <h1>Todo list</h1>
-      <Net/>
-      <Timer/>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={value}
-          onChange={e => setValue(e.target.value)}
-          required
-        />
-        <button type="submit">Add todo</button>
-      </form>
-      <section>
-        {todos.map((todo: ITodo, index: number) => (
-          <Fragment key={index}>
-            <div
-              style={{ textDecoration: todo.complete ? "line-through" : "" }}
-            >
-              {todo.text}
-            </div>
-            <button onClick={() => completTodos(index)}>
-              {todo.complete ? "incomplete" : "complete"}
-            </button>
-            <button type="button" onClick={() => removeTodo(index)}>
-              &times;
-            </button>
-          </Fragment>
-        ))}
-      </section>
-    </Fragment>
+    <Container fluid>
+      <Row style={{background: '#302464', height:60}}></Row>
+      <Row style={{background: '#302464'}}>
+        <Col>
+          <Row> upper row</Row>
+          <Row>
+          <Col>
+            <Row>row one</Row>
+            <Row style={{height:150,background: '#302464'}}>row two</Row>
+          </Col>
+          <Col style={{background: '#40427C'}}>
+            <Row><img src='flower.jpeg'/></Row>
+          </Col>
+        </Row>
+          <Row  style={{background: '#40327C'}}> lower row</Row>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row style={{background: '#40327C'}}>
+        <Col style={{background: '#40327C'}}>1 of 2</Col>
+      </Row>
+    </Container>
   );
 }
