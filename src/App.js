852f0ab63e03cae299abdb1bec3e42c812c35636
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-05-01 16:54:46 +0530

    row and col design basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello world!</h1>
+      <div>
+        <div className="row" style={{height: 75, background: '#302464'}}>
+        </div>
+        <div className="row no-gutters">
+          <div className="col-sm-1" style={{background: '#302464'}}>
+            <div className="row mySeparator no-gutters">
+            </div>
+            <div className="row no-gutters mySeparator2">
+            </div>
+          </div>
+          <div className="col-sm-4"><img className="mySelector" src="myimg.png"/></div>
+          <div className="col-sm-7" style={{background: '#302464'}}>
+            <div className="row mySeparator no-gutters">
+            </div>
+            <div className="row no-gutters mySeparator2">
+            </div>
+          </div>
+        </div>
+        <div>Hello</div>
       </div>
     );
   }
